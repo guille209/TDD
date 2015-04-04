688c5ffe9f01c40f056c7ca998e4c69a243ae012
Guille209 <Guille@GuilleDesktop>
2015-04-04 14:37:33 +0200

    Implementado metodotest deckisEmpty

--- src/test/MoveDeckToWasteControllerTest.java
+++ src/test/MoveDeckToWasteControllerTest.java
@@ -12,15 +12,23 @@ import solitario.MoveDeckToWasteController;
 
 public class MoveDeckToWasteControllerTest {
 
-	
 	private MoveDeckToWasteController moveDeckToWasteControllerTest;
 
-
 	@Before
-	public void before(){
+	public void before() {
 		Random r = new Random();
-		int deckCards = r.nextInt(29);
-		int wasteCards = r.nextInt(29);
-		moveDeckToWasteControllerTest = new MoveDeckToWasteController(deckCards,wasteCards);
+		int deckCards = (r.nextInt(30)) + 1;
+		int wasteCards = r.nextInt(30);
+		moveDeckToWasteControllerTest = new MoveDeckToWasteController(
+				deckCards, wasteCards);
+
 	}
+
+	@Test
+	public void deckIsEmpty() {
+		if (moveDeckToWasteController.initialDeckThreeOrLessCards()) {
+			assertTrue(moveDeckToWasteControllerTest.isDeckEmpty());
+		}
+	}
+
 }
