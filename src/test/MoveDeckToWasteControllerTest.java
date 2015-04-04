4b92ab62df281f7f8657c56862819d5a9cda11ef
Guille209 <Guille@GuilleDesktop>
2015-04-04 14:22:10 +0200

    Creado before del test MoveDeckTowasteControllerTest

--- src/test/MoveDeckToWasteControllerTest.java
+++ src/test/MoveDeckToWasteControllerTest.java
@@ -2,10 +2,24 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import solitario.Card;
 
 public class MoveDeckToWasteControllerTest {
 
 	
+	private MoveDeckToWasteController moveDeckToWasteControllerTest;
 
+
+	@Before
+	public void before(){
+		Random r = new Random();
+		int deckCards = r.nextInt(29);
+		int wasteCards = r.nextInt(29);
+		moveDeckToWasteControllerTest = new moveDeckToWasteController();
+	}
 }
