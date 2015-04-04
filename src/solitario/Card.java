8379d28c783a1f4fd23d0ea9e9c8c84f79d07a4e
Guille209 <Guille@GuilleDesktop>
2015-04-04 13:38:18 +0200

    Initial commit

---
+++ src/solitario/Card.java
@@ -0,0 +1,38 @@
+package solitario;
+
+public class Card {
+
+	private int suit;
+	private int number;
+	private boolean uncovered;
+
+	public Card(int suit, int number, boolean uncovered) {
+		this.suit = suit;
+		this.number = number;
+		this.uncovered = uncovered;
+
+	}
+
+	public boolean uncovered() {
+		return uncovered;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		assert object instanceof Card;
+		Card card = (Card) object;
+		return card.suit == suit && card.number == number;
+
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		if (uncovered)
+			return "CARD[P:" + suit + ",N:" + number + ",Descubierta]\n";
+		else
+			return "CARD[P:" + suit + ",N:" + number + ",Tapada]\n";
+
+	}
+
+}
